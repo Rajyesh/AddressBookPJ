0568687cf6a17919592cee287564901703bd6850
Virat-18 <rajeshspark74@gmail.com>
2024-10-26 11:07:40 +0530

    modified code

---
+++ file1.java
@@ -0,0 +1,24 @@
+class AddressBook {
+
+    static ArrayList<Contact> contacts = new ArrayList<>();
+    static Scanner scanner = new Scanner(System.in);
+
+    // UC 1: Add Contact
+    public static void addContact() {
+        System.out.print("Enter first name: ");
+        String firstName = scanner.next();
+        System.out.print("Enter last name: ");
+        String lastName = scanner.next();
+        String name = firstName + " " + lastName;
+        System.out.print("Enter ID: ");
+        String id = scanner.next();
+        System.out.print("Enter mobile number: ");
+        String mobileNumber = scanner.next();
+        System.out.print("Enter address: ");
+        String address = scanner.next();
+        System.out.print("Enter pincode: ");
+        String pincode = scanner.next();
+
+        contacts.add(new Contact(id, name, mobileNumber, address, pincode));
+    }
+}
